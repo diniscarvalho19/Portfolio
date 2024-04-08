9a3d47e807b268ba3e330fe75c24c90a6e10f8df
diniscarvalho19 <dinis113@gmail.com>
2024-04-08 18:24:04 +0100

    getting way better

--- resources/js/landing.js
+++ resources/js/landing.js
@@ -20,8 +20,24 @@ function updateTime() {
 
   hoursElement.textContent = date.getHours();
 
-  minutesElement.textContent = minutesFormattedTime;
+  minutesElement.textContent = minutesFormattedTime.padStart(2, "0");
 }
+
+const buttons = document.querySelectorAll('.about--navigation button');
+const aboutTextSections = document.querySelectorAll('.about--text');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    const section = this.dataset.section;
+
+    aboutTextSections.forEach(textSection => {
+      textSection.classList.remove('show'); 
+      if (textSection.id === section) {
+        textSection.classList.add('show'); 
+      }
+    });
+  });
+});
 
 updateTime();
 
